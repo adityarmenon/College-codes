f86beff1ea6f6e1baaef5b2b8695d71544f93b75
ADITYA R MENON <144932777+adityarmenon@users.noreply.github.com>
2024-09-28 20:19:15 +0530

    singlylinkedlistinsertionatbegining.c

---
+++ snglylinkedlistinsertionatbegining.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Insert node at the beginning
+void insertAtBeginning(struct Node** head, int newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Display the list
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d -> ", node->data);
+        node = node->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    insertAtBeginning(&head, 10);
+    insertAtBeginning(&head, 20);
+    insertAtBeginning(&head, 30);
+    
+    printList(head);
+    return 0;
+}
