482b8ed5987cbdac2e137904790fb797a060cdd4
ADITYA R MENON <144932777+adityarmenon@users.noreply.github.com>
2024-11-11 10:01:44 +0530

    Sparse.c

--- sparse.c
+++ sparse.c
@@ -1,58 +1,38 @@
 #include <stdio.h>
-
-void Sparse(int A[10][10], int m, int n) {
-    int i, j, k = 1, S[10][10];
-    
-    // Store the dimensions of the original matrix
+void createSparseMatrix(int A[10][10], int m, int n) {
+    int S[10][3], k = 1;
     S[0][0] = m;
     S[0][1] = n;
-    
-    // Convert the matrix to sparse representation
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
             if (A[i][j] != 0) {
                 S[k][0] = i;
                 S[k][1] = j;
                 S[k][2] = A[i][j];
                 k++;
-            }
-        }
-    }
-    
-    // Store the number of non-zero elements
+            }}}
     S[0][2] = k - 1;
-    
-    // Display the sparse matrix in tuple form
-    printf("\nTuple Form:\n");
-    for (i = 0; i <= S[0][2]; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%d\t", S[i][j]);
+    printf("\nSparse Matrix in Tuple Form:\n");
+    for (int i = 0; i <= S[0][2]; i++) {
+        printf("%d\t%d\t%d\n", S[i][0], S[i][1], S[i][2]);
+    }}
+int main() {
+    int A[10][10], m, n;
+    printf("Enter number of rows and columns: ");
+    scanf("%d%d", &m, &n);
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+     } }
+
+    printf("\nOriginal Matrix:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d\t", A[i][j]);
         }
         printf("\n");
     }
-}
-
-int main() {
-    int A[10][10];
-    int m, n, i, j;
-    
-    // Get matrix dimensions from the user
-    printf("Enter the number of rows: ");
-    scanf("%d", &m);
-    printf("Enter the number of columns: ");
-    scanf("%d", &n);
-    
-    // Get matrix elements from the user
-    printf("Enter the elements of the matrix:\n");
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
-            printf("A[%d][%d] = ", i, j);
-            scanf("%d", &A[i][j]);
-        }
-    }
-    
-    // Call the Sparse function to convert and display the matrix in tuple form
-    Sparse(A, m, n);
-    
+    createSparseMatrix(A, m, n);
     return 0;
 }
